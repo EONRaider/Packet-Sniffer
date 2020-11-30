d82ea785831bd7f3cdb27252f54bc9d52810ab15
EONRaider <livewire_voodoo@protonmail.com>
2020-11-30 09:14:44 -0300

    Refactor return of Protocol.addr_array_to_hdwr
    - Changed parameter names in addr_array_to_hdwr
    and hex_format for readability

--- protocols.py
+++ protocols.py
@@ -23,21 +23,21 @@ class Protocol(BigEndianStructure):
         return create_string_buffer(sizeof(self))[:]
 
     @staticmethod
-    def addr_array_to_hdwr(addr: str) -> str:
+    def addr_array_to_hdwr(address: str) -> str:
         """
         Converts a c_ubyte array of 6 bytes to IEEE 802 MAC address.
         Ex: From b'\xceP\x9a\xcc\x8c\x9d' to 'ce:50:9a:cc:8c:9d'
         """
-        return ':'.join('{:02x}'.format(octet) for octet in bytes(addr))
+        return ':'.join(format(octet, '02x') for octet in bytes(address))
 
     @staticmethod
-    def hex_format(value: int, str_len: int) -> str:
+    def hex_format(value: int, str_length: int) -> str:
         """
         Fills a hex value with zeroes to the left for compliance with
         the presentation of codes used in Internet protocols.
         Ex: From '0x800' to '0x0800'
         """
-        return format(value, '#0{}x'.format(str_len))
+        return format(value, '#0{}x'.format(str_length))
 
 
 class Ethernet(Protocol):      # IEEE 802.3 standard
