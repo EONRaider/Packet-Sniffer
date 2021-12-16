b5761365703c7d9e28efcd3acce0694320e9330b
EONRaider <livewire_voodoo@protonmail.com>
2021-12-16 19:37:37 -0300

    Move arp.py to layer2/

--- src/protocols/__init__.py
+++ src/protocols/__init__.py
@@ -1,6 +1,6 @@
 from src.protocols.protocol import Protocol
 from src.protocols.layer2.ethernet import Ethernet
-from src.protocols.layer3.arp import ARP
+from src.protocols.layer2.arp import ARP
 from src.protocols.layer3.icmp import ICMP
 from src.protocols.layer3.ip import IPv4, IPv6
 from src.protocols.layer4.tcp import TCP
