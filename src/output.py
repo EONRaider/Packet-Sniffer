3329dc9bf24600bd55ec07c1be6e1585bc6d5330
EONRaider <livewire_voodoo@protonmail.com>
2022-06-18 16:05:41 -0300

    Refactor OutputMethod class

--- src/output.py
+++ src/output.py
@@ -9,8 +9,8 @@ from abc import ABC, abstractmethod
 
 class OutputMethod(ABC):
     """Interface for the implementation of all classes responsible for
-    further processing and/or output of the information gathered by
-    the PacketSniffer class."""
+    further processing/output of the information gathered by the
+    PacketSniffer class."""
 
     i = " " * 4  # Basic indentation level
 
@@ -24,65 +24,94 @@ class OutputMethod(ABC):
 
 class OutputToScreen(OutputMethod):
     def __init__(self, subject, *, display_data: bool):
-        super().__init__(subject)
-        self._packet = None
-        self._display_data = display_data
+        """Output data from a decoded frame to screen.
 
-    def update(self, packet) -> None:
-        self._packet = packet
+        :param subject: Instance of PacketSniffer to be observed.
+        :param display_data: Boolean specifying the output of captured
+            data.
+        """
+        super().__init__(subject)
+        self._frame = None
+        self._display_data = display_data
+        self._initialize()
+
+    @staticmethod
+    def _initialize() -> None:
+        print("\n[>>>] Packet Sniffer initialized. Waiting for incoming "
+              "data. Press Ctrl-C to abort...\n")
+
+    def update(self, frame) -> None:
+        self._frame = frame
         self._display_output_header()
-        self._display_packet_info()
+        self._display_protocol_info()
         self._display_packet_contents()
 
     def _display_output_header(self) -> None:
         local_time = time.strftime("%H:%M:%S", time.localtime())
-        interface = "all" if self._packet.interface is None \
-            else self._packet.interface
-        print(f"[>] Packet #{self._packet.packet_num} at {local_time} from "
-              f"interface \"{interface}\":")
+        print(f"[>] Frame #{self._frame.packet_num} at {local_time}:")
 
-    def _display_packet_info(self) -> None:
-        for proto in self._packet.protocol_queue:
+    def _display_protocol_info(self) -> None:
+        """Iterate through a protocol queue and call the appropriate
+        display protocol method."""
+        for proto in self._frame.protocol_queue:
             getattr(self, f"_display_{proto.lower()}_data")()
 
     def _display_ethernet_data(self) -> None:
-        ethernet = self._packet.ethernet
+        ethernet = self._frame.ethernet
+        interface = "all" if self._frame.interface is None \
+            else self._frame.interface
+        frame_length: int = self._frame.frame_length
+        epoch_time: float = self._frame.epoch_time
         print(f"{self.i}[+] Ethernet {ethernet.src:.>23} -> {ethernet.dst}")
+        print(f"{2 * self.i}  Interface: {interface}")
+        print(f"{2 * self.i}  Frame length: {frame_length}")
+        print(f"{2 * self.i}  Epoch time: {epoch_time}")
 
     def _display_ipv4_data(self) -> None:
-        ipv4 = self._packet.ipv4
-        print(f"{self.i}[+] IPv4 {ipv4.src:.>27} -> {ipv4.dst: <15} | "
-              f"PROTO: {ipv4.encapsulated_proto} TTL: {ipv4.ttl}")
+        ipv4 = self._frame.ipv4
+        print(f"{self.i}[+] IPv4 {ipv4.src:.>27} -> {ipv4.dst: <15}")
+        print(f"{2 * self.i}  DSCP: {ipv4.dscp}")
+        print(f"{2 * self.i}  Total length: {ipv4.len}")
+        print(f"{2 * self.i}  ID: {ipv4.id}")
+        print(f"{2 * self.i}  Flags: {ipv4.flags_str}")
+        print(f"{2 * self.i}  TTL: {ipv4.ttl}")
+        print(f"{2 * self.i}  Protocol: {ipv4.encapsulated_proto}")
+        print(f"{2 * self.i}  Header checksum: {ipv4.chksum_hex_str}")
 
     def _display_ipv6_data(self) -> None:
-        ipv6 = self._packet.ipv6
+        ipv6 = self._frame.ipv6
         print(f"{self.i}[+] IPv6 {ipv6.src:.>27} -> {ipv6.dst: <15}")
+        print(f"{2 * self.i}  Traffic class: {ipv6.tclass_hex_str}")
+        print(f"{2 * self.i}  Flow label: {ipv6.flabel_txt_str}")
+        print(f"{2 * self.i}  Payload length: {ipv6.payload_len}")
+        print(f"{2 * self.i}  Next header: {ipv6.encapsulated_proto}")
+        print(f"{2 * self.i}  Hop limit: {ipv6.hop_limit}")
 
     def _display_arp_data(self) -> None:
-        arp = self._packet.arp
+        arp = self._frame.arp
         if arp.oper == 1:  # ARP Request
             print(f"{self.i}[+] ARP Who has {arp.tpa:.>18} ? -> Tell {arp.spa}")
         else:              # ARP Reply
             print(f"{self.i}[+] ARP {arp.spa:.>28} -> Is at {arp.sha}")
 
     def _display_tcp_data(self) -> None:
-        tcp = self._packet.tcp
+        tcp = self._frame.tcp
         print(f"{self.i}[+] TCP {tcp.sport:.>28} -> {tcp.dport: <15} | "
-              f"Flags: {tcp.flags_hex} > {tcp.flags_txt}")
+              f"Flags: {tcp.flags_hex_str} > {tcp.flags_str}")
 
     def _display_udp_data(self) -> None:
-        udp = self._packet.udp
-        print(f"{self.i}[+] UDP {udp.sport:.>27} -> {udp.dport}")
+        udp = self._frame.udp
+        print(f"{self.i}[+] UDP {udp.sport:.>28} -> {udp.dport}")
 
     def _display_icmp_data(self) -> None:
-        ipv4 = self._packet.ipv4
-        icmpv4 = self._packet.icmpv4
+        ipv4 = self._frame.ipv4
+        icmpv4 = self._frame.icmpv4
         print(f"{self.i}[+] ICMP {ipv4.sre:.>27} -> {ipv4.dst: <15} | "
               f"Type: {icmpv4.type_name}")
 
     def _display_packet_contents(self) -> None:
         if self._display_data is True:
             print(f"{self.i}[+] DATA:")
-            data = (self._packet.data.decode(errors="ignore").
+            data = (self._frame.data.decode(errors="ignore").
                     replace("\n", f"\n{self.i * 2}"))
             print(f"{self.i}{data}")
