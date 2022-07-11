7a37ecd29f798af0854db5b3aa12654517a2a34f
EONRaider <livewire_voodoo@protonmail.com>
2022-07-11 16:30:57 -0300

    Change script arguments
    The main executable was renamed from core.py to
    sniffer.py and a change was made to the arguments
    of pyinstaller to reflect the changes

--- build.py
+++ build.py
@@ -8,4 +8,4 @@ import PyInstaller.__main__ as pyinstaller
 
 """Set up the arguments required by PyInstaller to build the Network
 Packet Sniffer binary."""
-pyinstaller.run(("packet_sniffer/core.py", "--onefile"))
+pyinstaller.run(("net_sniffer/sniffer.py", "--onefile"))
