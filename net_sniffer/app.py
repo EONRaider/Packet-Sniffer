cc07f6a3097919404fd2d356c188d93c182974fa
EONRaider <livewire_voodoo@protonmail.com>
2022-07-14 12:25:01 -0300

    Rename file and extract function
    The file was renamed from sniffer.py to app.py.
    All the logic that was contained in the main
    execution context was refactored into new functions

---
+++ net_sniffer/app.py
@@ -0,0 +1,48 @@
+import argparse
+import os
+
+from net_sniffer.modules.sniffer import PacketSniffer
+from net_sniffer.output.screen import OutputToScreen
+
+
+def parse_cli_options():
+    parser = argparse.ArgumentParser(description="Network packet sniffer")
+    parser.add_argument(
+        "-i", "--interface",
+        type=str,
+        default=None,
+        help="Interface from which Ethernet frames will be captured (monitors "
+             "all available interfaces by default)."
+    )
+    parser.add_argument(
+        "-d", "--data",
+        action="store_true",
+        help="Output packet data during capture."
+    )
+    return parser.parse_args()
+
+
+def run():
+    args = parse_cli_options()
+
+    if os.getuid() != 0:
+        raise SystemExit("Error: Permission denied. This application requires "
+                         "administrator privileges to run.")
+
+    OutputToScreen(
+        subject=(sniffer := PacketSniffer()),
+        display_data=args.data
+    )
+
+    try:
+        for _ in sniffer.listen(args.interface):
+            '''Iterate through the frames yielded by the listener in an 
+            infinite cycle while feeding them to all registered observers 
+            for further processing/output'''
+            pass
+    except KeyboardInterrupt:
+        raise SystemExit("[!] Aborting packet capture...")
+
+
+if __name__ == '__main__':
+    run()
