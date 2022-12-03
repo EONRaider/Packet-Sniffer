2580f605824350e22d21b7d85a161e10bb6eac96
eonraider <livewire_voodoo@protonmail.com>
2022-12-03 17:08:32 -0300

    Add banner

--- net_sniffer/cli_parser.py
+++ net_sniffer/cli_parser.py
@@ -5,11 +5,28 @@ __author__ = "EONRaider @ keybase.io/eonraider"
 
 import argparse
 
+_banner = r"""
+    _   ___________________       _ ________         
+   / | / / ____/_  __/ ___/____  (_) __/ __/__  _____
+  /  |/ / __/   / /  \__ \/ __ \/ / /_/ /_/ _ \/ ___/
+ / /|  / /___  / /  ___/ / / / / / __/ __/  __/ /    
+/_/ |_/_____/ /_/  /____/_/ /_/_/_/ /_/  \___/_/     
+
+A network packet sniffer that disassembles packets upon
+arrival at a given network interface controller and
+displays their information on screen.
+
+             Developed by EONRaider
+   https://github.com/EONRaider/Packet-Sniffer
+
+"""
+
 
 class CLIParser:
     def __init__(self):
         self.parser = argparse.ArgumentParser(
-            description="Network packet sniffer"
+            description=_banner,
+            formatter_class=argparse.RawDescriptionHelpFormatter,
         )
 
     def parse(self) -> argparse.Namespace:
@@ -28,4 +45,3 @@ class CLIParser:
             help="Output packet data during capture.",
         )
         return self.parser.parse_args()
-
